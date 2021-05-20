70188f7962120d489180df8b6c87dbde6b4b9962
root <root@mailtool.csi.it>
2021-05-20 10:59:23 +0200

    - update LDAP ldif. - For a kind of mistake, I had disabled the main form. Fixed.

--- index.php
+++ index.php
@@ -51,8 +51,7 @@ if ( $dkim['selector']['separator'] === '_' )
 <td>Domain</td><td><input maxlength="255" value="" type="text" name="domain" placeholder="RFC5322.From domain"
                 pattern="\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b"
                 title="Look at your syntax. You must insert a FQDN domain name."
-		required
-		disabled="disabled">
+		required>
 <input type="submit" 
        style="position: absolute; left: -9999px; width: 1px; height: 1px;"
        tabindex="-1" />
